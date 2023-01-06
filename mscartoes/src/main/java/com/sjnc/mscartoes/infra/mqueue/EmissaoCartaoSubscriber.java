58e739eb7f7a51f06118dfee8f7dd62bebcb5830
saulocapistrano <saulocapistrano@gmail.com>
2023-01-06 15:35:49 -0300

    correrção mscartaosubscrible

--- mscartoes/src/main/java/com/sjnc/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ mscartoes/src/main/java/com/sjnc/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -19,24 +19,24 @@ public class EmissaoCartaoSubscriber {
     private final CartaoRepository cartaoRepository;
     private final ClienteCartaoRepository clienteCartaoRepository;
 
-    @RabbitListener(queues = "${mq.queues.emissao-cartoes:}")
+    @RabbitListener(queues = "${mq.queues.emissao-cartoes}")
     public void receberSolicitacaoEmissao(@Payload String payload){
+       //System.out.println(payload);
+        try {
+            var mapper = new ObjectMapper();
 
-       try {
-           var mapper = new ObjectMapper();
-
-           DadosSolicitacaoEmissaoCartao dados = mapper.readValue(payload, DadosSolicitacaoEmissaoCartao.class);
-           Cartao cartao = cartaoRepository.findById(dados.getIdCartao()).orElseThrow();
-           ClienteCartao clienteCartao = new ClienteCartao();
-           clienteCartao.setCartao(cartao);
-           clienteCartao.setCpf(dados.getCpf());
-           clienteCartao.setLimite(dados.getLimiteLiberado());
+            DadosSolicitacaoEmissaoCartao dados = mapper.readValue(payload, DadosSolicitacaoEmissaoCartao.class);
+            Cartao cartao = cartaoRepository.findById(dados.getIdCartao()).orElseThrow();
+            ClienteCartao clienteCartao = new ClienteCartao();
+            clienteCartao.setCartao(cartao);
+            clienteCartao.setCpf(dados.getCpf());
+            clienteCartao.setLimite(dados.getLimiteLiberado());
 
 
-           clienteCartaoRepository.save(clienteCartao);
+            clienteCartaoRepository.save(clienteCartao);
 
         }catch(Exception e){
             e.printStackTrace();
         }
-       }
+    }
 }
